99ae67b18788370ee2e41fea9bb9759445a27d8d
Mwolf <aosyang@hotmail.com>
2014-12-11 02:45:04 -0800

    Reverted smallpt source code

--- RayTracer/RayTracerThread.mm
+++ RayTracer/RayTracerThread.mm
@@ -8,11 +8,6 @@
 
 #import "RayTracerThread.h"
 
-#include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
-#include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
-#include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
-#include <memory.h>
-
 struct Vec {        // Usage: time ./smallpt 5000 && xv image.ppm
     double x, y, z;                  // position, also color (r,g,b)
     Vec(double x_=0, double y_=0, double z_=0){ x=x_; y=y_; z=z_; }
@@ -39,15 +34,7 @@ struct Sphere {
         return (t=b-det)>eps ? t : ((t=b+det)>eps ? t : 0);
     }
 };
-
-@implementation RayTracerThread
-
-static int width = 400;
-static int height = 400;
-static unsigned char* data;
-static NSLock* threadLock;
-
-static Sphere spheres[] = {//Scene: radius, position, emission, color, material
+Sphere spheres[] = {//Scene: radius, position, emission, color, material
     Sphere(1e5, Vec( 1e5+1,40.8,81.6), Vec(),Vec(.75,.25,.25),DIFF),//Left
     Sphere(1e5, Vec(-1e5+99,40.8,81.6),Vec(),Vec(.25,.25,.75),DIFF),//Rght
     Sphere(1e5, Vec(50,40.8, 1e5),     Vec(),Vec(.75,.75,.75),DIFF),//Back
@@ -58,9 +45,6 @@ static Sphere spheres[] = {//Scene: radius, position, emission, color, material
     Sphere(16.5,Vec(73,16.5,78),       Vec(),Vec(1,1,1)*.999, REFR),//Glas
     Sphere(600, Vec(50,681.6-.27,81.6),Vec(12,12,12),  Vec(), DIFF) //Lite
 };
-
-//static Sphere *spheres;
-
 inline double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; }
 inline int toInt(double x){ return int(pow(clamp(x),1/2.2)*255+.5); }
 inline bool intersect(const Ray &r, double &t, int &id){
@@ -75,7 +59,7 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi){
     const Sphere &obj = spheres[id];        // the hit object
     Vec x=r.o+r.d*t, n=(x-obj.p).norm(), nl=n.dot(r.d)<0?n:n*-1, f=obj.c;
     double p = f.x>f.y && f.x>f.z ? f.x : f.y>f.z ? f.y : f.z; // max refl
-    if (++depth>5) {if (erand48(Xi)<p) f=f*(1/p); else return obj.e;} //R.R.
+    if (++depth>5) if (erand48(Xi)<p) f=f*(1/p); else return obj.e; //R.R.
     if (obj.refl == DIFF){                  // Ideal DIFFUSE reflection
         double r1=2*M_PI*erand48(Xi), r2=erand48(Xi), r2s=sqrt(r2);
         Vec w=nl, u=((fabs(w.x)>.1?Vec(0,1):Vec(1))%w).norm(), v=w%u;
@@ -93,8 +77,15 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi){
     double Re=R0+(1-R0)*c*c*c*c*c,Tr=1-Re,P=.25+.5*Re,RP=Re/P,TP=Tr/(1-P);
     return obj.e + f.mult(depth>2 ? (erand48(Xi)<P ?   // Russian roulette
                                      radiance(reflRay,depth,Xi)*RP:radiance(Ray(x,tdir),depth,Xi)*TP) :
-                          radiance(reflRay,depth,Xi)*Re+radiance(Ray(x,tdir),depth,Xi)*Tr);
+                          radiance(reflRay,depth,Xi)*Re+radiance(Ray(x,tdir),depth,Xi)*Tr); 
 }
+
+@implementation RayTracerThread
+
+static int width = 400;
+static int height = 400;
+static unsigned char* data;
+static NSLock* threadLock;
 
 
 + (void)ThreadMain:(id)param
